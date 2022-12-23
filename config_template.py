4243180956d51302b008bbe9f3f8ca8f546ab553
Erik Neemann <eneemann@utah.gov>
2022-12-23 15:39:49 -0700

    feat: add config file

---
+++ config_template.py
@@ -0,0 +1,12 @@
+"""
+config.py: Configuration values
+"""
+#: Set variables
+TESSDATA = r"" #: path to tessdata directory
+TESSERACT_EXE = r"" #: path to tesseract executable
+
+#: Set Tesseract options
+#: These characters are not used: I, O,
+wl = "" #: characters that are explicity allowed
+bl = ""     #: characters that are explicitly not allowed
+OCR_CONFIG = ""
