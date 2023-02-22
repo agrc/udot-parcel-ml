9a4f3076f682fe8fedc38a867138b4e659f94d81
steveoh <sgourley@utah.gov>
2023-02-21 17:24:34 -0700

    refactor: switch to getenv and put all variables together

--- row_run.py
+++ row_run.py
@@ -5,7 +5,7 @@ the file to run to start the project
 """
 
 import logging
-from os import environ
+from os import getenv
 from sys import stdout
 from time import perf_counter
 from types import SimpleNamespace
@@ -21,28 +21,30 @@ logging.basicConfig(
 )
 
 #: Set up variables
-INDEX = environ["INDEX_FILE_LOCATION"]
-BUCKET_NAME = environ["INPUT_BUCKET"]
-OUTPUT_BUCKET_NAME = environ["OUTPUT_BUCKET"]
-JOB_TYPE = environ["JOB_TYPE"]
-JOB_NAME = environ["JOB_NAME"]
+INDEX = getenv("INDEX_FILE_LOCATION")
+BUCKET_NAME = getenv("INPUT_BUCKET")
+OUTPUT_BUCKET_NAME = getenv("OUTPUT_BUCKET")
+JOB_TYPE = getenv("JOB_TYPE")
+JOB_NAME = getenv("JOB_NAME")
+TASK_INDEX = int(getenv("CLOUD_RUN_TASK_INDEX") or 0)
+TASK_COUNT = int(getenv("CLOUD_RUN_TASK_COUNT") or 0)
+TOTAL_FILES = int(getenv("TOTAL_FILES") or 0)
+QUOTA = int(getenv("QUOTA") or 1)
+PROJECT_NUMBER = int(getenv("PROJECT_NUMBER") or 0)
+PROCESSOR_ID = getenv("PROCESSOR_ID")
 
 
 def mosaic_all_circles():
     """the main function to execute when cloud run starts the circle detection job"""
 
-    task_index = int(environ["CLOUD_RUN_TASK_INDEX"])
-    task_count = int(environ["CLOUD_RUN_TASK_COUNT"])
-    total_files = int(environ["TOTAL_FILES"])
-
     job_start = perf_counter()
 
-    row.mosaic_all_circles(JOB_NAME, BUCKET_NAME, OUTPUT_BUCKET_NAME, INDEX, task_index, task_count, total_files)
+    row.mosaic_all_circles(JOB_NAME, BUCKET_NAME, OUTPUT_BUCKET_NAME, INDEX, TASK_INDEX, TASK_COUNT, TOTAL_FILES)
 
     logging.info(
         "job name: %s task %i: entire job %s",
         JOB_NAME,
-        task_index,
+        TASK_INDEX,
         row.format_time(perf_counter() - job_start),
     )
 
@@ -56,9 +58,9 @@ def ocr_all_mosaics():
         input_bucket=BUCKET_NAME,
         output_location=OUTPUT_BUCKET_NAME,
         file_index=INDEX,
-        quota=int(environ["QUOTA"]),
-        project_number=int(environ["PROJECT_NUMBER"]),
-        processor_id=environ["PROCESSOR_ID"],
+        quota=QUOTA,
+        project_number=PROJECT_NUMBER,
+        processor_id=PROCESSOR_ID,
     )
 
     row.ocr_all_mosaics(inputs)
